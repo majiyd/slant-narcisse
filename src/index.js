a2eaae37038c119e206455e46a3df20c07c9e36c
majiyd <suleimanmajiyd@gmail.com>
2019-03-19 19:19:12 +0100

    set up files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Profile extends React.Component{
+	render(){
+		return(
+			
+		);
+	}
+}
+
+ReactDOM.render(
+  <Profile />,
+  document.getElementById('root')
+);
