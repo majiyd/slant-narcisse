be813a28a3a2e945ce935108853f0bec34e31019
majiyd <suleimanmajiyd@gmail.com>
2019-03-21 21:17:11 +0100

    finished touches to empty recs

--- src/components/profilePageBody.js
+++ src/components/profilePageBody.js
@@ -1,13 +1,52 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons';
 
 function Sidebar(props){
+    return (
+      <div className="sidebar">
+        <div className="sidebar-content">
+          <div className="sidebar-section">
+            <a className="sidebar-link selected" href="#"> Recs (1)</a>
+            <a className="sidebar-link" href="#"> Karma</a>
+            <a className="sidebar-link" href="#"> Activity</a>
+          </div>
+        </div>
+      </div>
+    );
+}
+function NoRecs(props){
+    return (
+      <div className="empty-state compact">
+        <div className="empty-state-icon">
+          <FontAwesomeIcon
+            className="icon thumbtack-icon"
+            icon={faThumbtack}
+          />
+        </div>
+        <span >No pinned recommendations yet!</span>
+      </div>
+    );
+}
+function RecsView(props){
     return(
-        <div className='sidebar'>
-            <div className='sidebar-content'>
-                
+        <div className="profile-page-recs-view">
+            <div className="pinned-recs card-list">
+                <div className="profile-page-title card-list-section-label">
+                    MY PINNED RECS (0)
+                </div>
+                <div className="recs-view-section">
+                    <NoRecs />
+                </div>
             </div>
         </div>
-    );
+    )
+}
+function KarmaView(props) {
+
+}
+function ActivityView(props) {
+
 }
 export class ProfilePageBody extends React.Component{
     render(){
@@ -15,6 +54,9 @@ export class ProfilePageBody extends React.Component{
             <div className="profile-page-body">
                 <div className="page-content profile-page-content">
                     <Sidebar />
+                    <div className='profile-sections-wrapper'>
+                        <RecsView />
+                    </div>
                 </div>
             </div>
         );
