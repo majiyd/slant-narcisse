0b2cef9af6345d94f872f77d6ef4ead835c97421
majiyd <suleimanmajiyd@gmail.com>
2019-03-31 17:13:59 +0100

    home page sidebar design completed

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -49,12 +49,48 @@ function HomePageHeader(props){
         </div>
     )
 }
+function NavSideBar(props){
+    return(
+        <div className="nav-sidebar left">
+            <div className="nav-sidebar-content">
+                <div className="nav-sidebar-section">
+                    <a className="nav-sidebar-link selected" href="#">Recommendations</a>
+                    <a className="nav-sidebar-link" href="#">Questions</a>
+                    <a className=" nav-sidebar-link" href="#">Top</a>
+                    <a className="nav-sidebar-link" href="#">Improve</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+function HomePageView(props){
+    return(
+        <div className="home-page-view">
+            <div className="nav-header scrollable show-mobile" >
+                <div className="nav-header-content page-content">
+                    <div className="nav-header-list">
+                        <a className="nav-header-item selected" href="#">Recommendations</a>
+                        <a className=" nav-header-item" href="#">Questions</a>
+                        <a className=" nav-header-item" href="#">Top</a>
+                        <a className=" nav-header-item" href="#">Improve</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="page-content">
+                    <NavSideBar />
+                </div>
+            </div>
+        </div>
+    );
+}
 export default class HomePage extends React.Component{
     render(){
         return(
             <div className="home-page">
                 <MainHeader />
                 <HomePageHeader />
+                <HomePageView />
             </div>
         )
     }
